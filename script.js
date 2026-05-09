69a60ea93efa86169768e083b8f5153042988e70
iknanyaasin <aayanjaman07@gmail.com>
2026-05-09 14:43:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -371,3 +371,12 @@ function updateCartCount() {
 }
 
 updateCartCount();
+
+function toggleMenu() {
+
+    const navLinks = document.getElementById("navLinks");
+    const hamburger = document.querySelector(".hamburger");
+
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+}
